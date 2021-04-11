2c6b65d25b841a3cd49e34cd1a43e3442b32bd8b
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-11 16:25:04 +0700

    delete commented code

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -2,23 +2,29 @@ import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
 
 import { CategoryService } from '../category/category.service'
-import { CategoryForm } from '../category/category' 
+import { CategoryForm , QuestionForm} from '../category/category' 
 
 @Component({
   selector: 'ns-play',
   templateUrl: './play.component.html',
 })
-export class PlayComponent implements OnInit {
-  selected_category : CategoryForm
 
-  constructor( private categoryService : CategoryService, private route: ActivatedRoute ) {}
+export class PlayComponent implements OnInit {
+  title = "Let's Quiz";
+  selected_category : CategoryForm;
+
+  constructor( private categoryService : CategoryService, private route: ActivatedRoute ) {
+  }
 
   ngOnInit(): void {
     const id = +this.route.snapshot.params.id
     this.selected_category = this.categoryService.getSelectedCategoryData(id);
   }
   getTotalQuestions(){
-    return console.log(this.selected_category.name);
+    console.log(this.selected_category.questions);
+    return this.selected_category.questions.length;
+  }
+  selectedQuestion(){
+    }
   }
   
-}
