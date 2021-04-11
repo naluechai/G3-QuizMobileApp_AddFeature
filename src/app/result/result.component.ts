2621e065a057cf3ddb0e0d18b60311570d7b461d
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-12 00:06:24 +0700

    Result template can show score

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ns-result',
   templateUrl: './result.component.html',
 })
 export class ResultComponent implements OnInit {
-  constructor() {}
+  result:number;
+  
+  constructor(private route: ActivatedRoute) {}
   ngOnInit(): void {
+    this.result = +this.route.snapshot.params.score
   }
 }
