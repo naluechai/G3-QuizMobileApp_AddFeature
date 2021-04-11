744560720eb20ed2611113f94179e115099ecb14
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-11 22:43:04 +0700

    1 Button can Go to Result

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { ActivatedRoute } from '@angular/router'
+import { Router } from '@angular/router';
 
 import { CategoryService } from '../category/category.service'
 import { CategoryForm , QuestionForm} from '../category/category' 
@@ -13,26 +14,33 @@ export class PlayComponent implements OnInit {
   title = "Let's Quiz";
   selected_category : CategoryForm;
   number:number = 0;
+  answerList:Array<any>;
   selected_question :QuestionForm;
-  constructor( private categoryService : CategoryService, private route: ActivatedRoute ) {
-  }
+
+  constructor( private categoryService : CategoryService, private route: ActivatedRoute, private router : Router ) {}
 
   ngOnInit(): void {
     const id = +this.route.snapshot.params.id
     this.selected_category = this.categoryService.getSelectedCategoryData(id);
+    this.answerList = this.selected_category.answer;
     this.selectedQuestion();
   }
-  getTotalQuestions(){
-    console.log(this.selected_question.question);
+  getConsoleLog(){
+    console.log(this.answerList);
     //return this.selected_category.questions;
   }
   selectedQuestion(){
     console.log(this.selected_category.questions[this.number]);
+
     this.selected_question = this.selected_category.questions[this.number];
     this.number+=1;
-    if (this.number ==this.selected_category.questions.length){
-      this.number = 0;
+    if (this.number == this.selected_category.questions.length+1){
+      //this.number = 0;
+      this.router.navigate(['/result']);
+      }
     }
-    }
+  collectSelected(){
+    
+  }
   }
   
