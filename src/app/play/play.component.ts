4962e09d5f58fe36b48d68cd4356e2a4c8b5c61d
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-13 21:24:43 +0700

    Improve Model That Collect Answer in Category

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -13,12 +13,11 @@ import { CategoryForm , QuestionForm} from '../category/category'
 export class PlayComponent implements OnInit {
   title = "Let's Quiz";
   selected_category : CategoryForm;
-  selected_question :QuestionForm;
+  selected_question :QuestionForm; //A moment Question that show
   answerList:Array<any>;
   lengthQuestion:number ;
   number:number = 0;
   score:number = 0;
-  selectedAnswer = [];
   tempSelectedAnswer = 0; 
 
   constructor( private categoryService : CategoryService, private route: ActivatedRoute, private router : Router ) {}
@@ -26,43 +25,40 @@ export class PlayComponent implements OnInit {
   ngOnInit(): void {
     const id = +this.route.snapshot.params.id
     this.selected_category = this.categoryService.getSelectedCategoryData(id);
-    this.answerList = this.selected_category.answer;
-    this.lengthQuestion = this.selected_category.answer.length;
+    this.lengthQuestion = this.selected_category.questions.length;
     this.selectedQuestion();
   }
   getConsoleLog(){
     console.log(this.answerList);
-    //return this.selected_category.questions;
   }
   selectedQuestion(){
       //console.log(this.selected_category.questions[this.number]);//undefined if last question
       if (this.number == this.lengthQuestion){//last question
-        //this.number = 0;
-        this.selectedAnswer.push(this.tempSelectedAnswer);
-        this.router.navigate(['/result',this.selected_category.id,this.checkScore(this.score)]);
+        this.checkAnswer();
+        //this.selectedAnswer.push(this.tempSelectedAnswer);
+        this.router.navigate(['/result',this.selected_category.id,this.score]);
         }
       else if (this.tempSelectedAnswer == 0 ){//start first question
         this.selected_question = this.selected_category.questions[this.number];
         this.number+=1;
       }
       else {//next question 
-        this.selectedAnswer.push(this.tempSelectedAnswer);
+        this.checkAnswer();
+        //this.selectedAnswer.push(this.tempSelectedAnswer);
         this.selected_question = this.selected_category.questions[this.number];
         this.number+=1;
       }
-
+      console.log("Score",this.score)
     }
   collectSelected( id:number ){//select answer
     this.tempSelectedAnswer = id;
-    console.log(this.tempSelectedAnswer);
   }
-  checkScore( score:number):number{
-    for (let i = 0 ; i < this.selectedAnswer.length ;i++){
-      if(this.selectedAnswer[i] == this.answerList[i].answer){
-        score +=1;
-      }
+  checkAnswer(){//every time click next button (use selectedQuestion) check answer u click with ans if same +1
+    if(this.tempSelectedAnswer == this.selected_question.ans){
+      this.score +=1
     }
-    return score;
+    this.score +=0
+    
   }
   }
   
