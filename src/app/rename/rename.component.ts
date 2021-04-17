a14444ec0414beb37d51d5c03f77370fd5a03053
Charnchanok Thuppasutthi <s6201012620058@email.kmutnb.ac.th>
2021-04-17 21:12:04 +0700

    Able to Edit Catefory Question Choice Name

--- src/app/rename/rename.component.ts
+++ src/app/rename/rename.component.ts
@@ -20,46 +20,40 @@ export class RenameComponent implements OnInit {
     this.CategoryID = +this.route.snapshot.params.id
     this.QuestionID = +this.route.snapshot.params.questionId
     this.ChoiceID = +this.route.snapshot.params.choiceId
-    console.log(this.CategoryID,  this.QuestionID, this.ChoiceID)
+    console.log(this.CategoryID, this.QuestionID, this.ChoiceID)
     this.checkData()
   }
+  // Check NaN with boolean will return False
   checkData(){
-    if ( this.QuestionID !=NaN && this.ChoiceID != NaN  ){  
-      console.log("1")
-      var ChoiceData = this.categoryService.getSelectedChoiceData(this.CategoryID, this.QuestionID, this.ChoiceID)
-      this.SelectedData = ChoiceData.choice 
-      
+    if ( Boolean(this.QuestionID) == false && Boolean(this.ChoiceID) == false ){  
+      var CategoryData = this.categoryService.getSelectedCategoryData(this.CategoryID)   
+      this.SelectedData = CategoryData.name
     }
-    else if ( this.QuestionID != NaN){
-      console.log("2")
+    else if ( Boolean(this.QuestionID) === true && Boolean(this.ChoiceID) == false ) {
       var QuestionData = this.categoryService.getSelectedQuestionData(this.CategoryID, this.QuestionID)   
       this.SelectedData = QuestionData.question
     }
-    else {
-      console.log("3")
-      var CategoryData = this.categoryService.getSelectedCategoryData(this.CategoryID)   
-      this.SelectedData = CategoryData.name
+    else { 
+      var ChoiceData = this.categoryService.getSelectedChoiceData(this.CategoryID, this.QuestionID, this.ChoiceID)
+      this.SelectedData = ChoiceData.choice 
     }
     return this.SelectedData
   }
 
   edit(){
     if(this.SelectedData !="" && this.SelectedData != undefined){
-      console.log(this.CategoryID, this.QuestionID, this.CategoryID, this.SelectedData)
-      if( this.ChoiceID != NaN){
-        console.log("4")
-        
-        this.categoryService.editChoiceName(this.CategoryID, this.QuestionID, this.ChoiceID , this.SelectedData)
+      if( Boolean(this.QuestionID) == false && Boolean(this.ChoiceID) == false  ){
+        this.categoryService.editCategoryName(this.CategoryID, this.SelectedData)
+        this.router.navigate(['/detail',this.CategoryID]);
       }
-      else if ( this.QuestionID !=NaN && this.ChoiceID != NaN){
-        console.log("5")
+      else if ( Boolean(this.QuestionID) === true && Boolean(this.ChoiceID) == false  ){
         this.categoryService.editQuestionName(this.CategoryID, this.QuestionID, this.SelectedData)
+        this.router.navigate(['/detail',this.CategoryID, this.QuestionID]);
       }
       else {
-        console.log("6")
-        this.categoryService.editCategoryName(this.CategoryID, this.SelectedData)
+        this.categoryService.editChoiceName(this.CategoryID, this.QuestionID, this.ChoiceID , this.SelectedData)
+        this.router.navigate(['/detail',this.CategoryID, this.QuestionID]);
       }
-      this.router.navigate(['/detail',this.CategoryID, this.QuestionID]);
     }
   }
 }
