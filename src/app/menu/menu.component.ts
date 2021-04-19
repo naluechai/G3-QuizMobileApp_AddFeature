71817240462c61c1b716c85cf63ed7618fcfe3c5
film6090 <68315089+film6090@users.noreply.github.com>
2021-04-19 14:07:19 +0700

    main menu change

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,7 +5,6 @@ import { ActivatedRoute } from '@angular/router'
 @Component({
   selector: 'ns-menu',
   templateUrl: './menu.component.html',
-  styleUrls:['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
   
